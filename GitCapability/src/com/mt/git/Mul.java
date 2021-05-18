8d8db15740985396e1c7e7839de9e5bcd05b2fd9
SwaroopReddy <swaroop8787@gmail.com>
2021-05-18 21:48:25 +0530

    d variable added again

--- GitCapability/src/com/mt/git/Mul.java
+++ GitCapability/src/com/mt/git/Mul.java
@@ -3,8 +3,8 @@ package com.mt.git;
 public class Mul {
 	public static void main(String[] args) {
 
-		int a=10,b=20,c=10;
-		System.out.println("mul : "+a*b*c);
+		int a=10,b=20,c=10,d=40;
+		System.out.println("mul : "+a*b*c*d);
 
 	}
 }
